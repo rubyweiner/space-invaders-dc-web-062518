c10f787f7ec535b6812ec197c8a3c254792fc6d7
Ruby Weiner <rubyweiner796@me.com>
2018-08-14 12:05:58 -0400

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,27 @@
-class CrewMember {}
+
+class CrewMember {
+  constructor(position){
+    this.position = position;
+    this.currentShip = "Looking for a Rig";
+  }
+
+  engageWarpDrive(){
+    if (this.currentShip === "Looking for a Rig"){
+      return "had no effect";
+    }
+  }
+
+  setsInvisibility(){
+    if (this.position === "Defender" && this.currentShip != "Looking for a Rig"){
+      this.currentShip.cloaked = true
+    } else {
+      return "had no effect";
+    }
+  }
+
+  chargePhasers(){
+    if (this.currentShip === "Looking for a Rig"){
+      return "had no effect";
+    }
+  }
+}
--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,25 @@
-class Spaceship {}
+class Spaceship {
+  constructor(name, crew, phasers, shields) {
+    this.name = name;
+    this.crew = crew;
+    this.phasers = 5;
+    this.shields = 4;
+    this.cloaked = false;
+    this.warpDrive = "disengaged";
+    this.phasersCharge = "uncharged";
+
+    if (this.crew.length){
+      this.docked = false;
+    } else {
+      this.docked = true;
+    }
+
+    this.notifyCrew();
+
+  }
+
+  notifyCrew() {
+    this.crew.forEach(crewMember => (crewMember.currentShip = this));
+  }
+
+}
